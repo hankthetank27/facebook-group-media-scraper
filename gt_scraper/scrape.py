e08bbef2ae5d2b1c7360c641bfaa710d5367a746
Hank Jackson <hjackson277@gmail.com>
2023-02-18 20:17:22 -0500

    fetching basic info on small set

--- gt_scraper/scrape.py
+++ gt_scraper/scrape.py
@@ -6,7 +6,6 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.service import Service as FirefoxService
-from selenium.webdriver.firefox.options import Options
 from webdriver_manager.firefox import GeckoDriverManager
 
 driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
@@ -22,19 +21,18 @@ def login():
     driver.find_element(By.CSS_SELECTOR, '[name="login"]').click()
 
 def goToGroup():
-    driver.get('https://www.facebook.com/groups/200453430055131')
+    driver.get(f'https://m.facebook.com/groups/200453430055131')
 
 def retrivePosts():
-    # Get scroll height.
+
     last_height = driver.execute_script("return document.body.scrollHeight")
     count = 0
     while True and count < 1:
 
-        # Scroll down to the bottom.
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
         # Wait to load the page.
-        time.sleep(2)
+        time.sleep(5)
         #or...
         #wait.until(EC. ....)
 
@@ -50,33 +48,57 @@ def retrivePosts():
         #remove
         count += 1
 
-    return driver.find_elements(By.CSS_SELECTOR, '[role="article"]')
+    posts = driver.find_element(By.ID, 'm_group_stories_container').find_elements(By.TAG_NAME, 'article')
 
-def parseFeed(posts):
-       
     for post in posts:
-        links = post.find_elements(By.TAG_NAME, 'a')
-        for link in links:
-            
-            print(link.get_attribute('href'))
+        user_name = lookupXpath(post, './/div/header/div/div[2]/div/div/div/div[1]/h3/span/strong[1]/a')
+        track_title = lookupXpath(post, './/div/div[2]/section/section/div/div/header/h3/span/span')
+
+        post_data = {}
+        if user_name:
+            post_data['user_name'] = user_name.text
+        if track_title:
+            post_data['track_title'] = track_title.text
+
+        print(post_data)
+
+def lookupXpath(partent, path):
+    try:
+        return partent.find_element('xpath', path)
+    except:
+        return None
+
+login()
+goToGroup()
+posts = retrivePosts()
 
 
-#feed
-'/html/body/div[1]/div/div[1]/div/div[3]/div/div/div/div[1]/div[1]/div/div[2]/div/div/div[4]/div/div[2]/div/div/div[1]/div[2]/div'
+#!/usr/bin/env python3
+# from selenium import webdriver
+# from selenium.common.exceptions import *
+# from selenium.webdriver.chrome.options import Options
+# from webdriver_manager.chrome import ChromeDriverManager
+# import time
 
+# chrome_options = Options()
+# chrome_options.add_argument("--headless")
+# driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
 
-#fist item
-'/html/body/div[1]/div/div[1]/div/div[3]/div/div/div/div[1]/div[1]/div/div[2]/div/div/div[4]/div/div[2]/div/div/div[1]/div[2]/div/div[2]'
-
-#article
-'/html/body/div[1]/div/div[1]/div/div[3]/div/div/div/div[1]/div[1]/div/div[2]/div/div/div[4]/div/div[2]/div/div/div[1]/div[2]/div/div[2]/div/div/div/div'
-
-#link
-'/html/body/div[1]/div/div[1]/div/div[3]/div/div/div/div[1]/div[1]/div/div[2]/div/div/div[4]/div/div[2]/div/div/div[1]/div[2]/div/div[2]/div/div/div/div/div/div/div/div/div/div[8]/div/div/div[3]/div[2]/div[1]/div[1]/div/a'
-
-
-
-# login()
-# goToGroup()
-# posts = retrivePosts()
-# parseFeed(posts)
+# driver.get(f"https://www.facebook.com/groups/120778514747417")
+# driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+# time.sleep(1)
+# posts = driver.find_elements_by_css_selector("div[role='article']")
+# for post in posts:
+#     labelled_by = post.get_attribute("aria-labelledby")
+#     print(labelled_by)
+#     label = driver.find_element_by_id(labelled_by).text
+#     print(label)
+#     desc_by = post.get_attribute("aria-describedby").split()
+#     for desc_elem in desc_by:
+#         print(desc_elem)
+#         try:
+#             label = driver.find_element_by_id(desc_elem).text
+#             if label:
+#                 print(label.strip())
+#         except NoSuchElementException:
+#             pass
